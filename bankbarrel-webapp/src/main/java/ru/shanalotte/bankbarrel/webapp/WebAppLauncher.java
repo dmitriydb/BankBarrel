4b2f44e51539c45ceaeddf83920b049eece8007a
Dmitriy Drizhilo <dmitryd@logicon-group.ru>
2022-04-22 15:21:34 +0500

    Logging & devtools enabled.

--- bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/WebAppLauncher.java
+++ bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/WebAppLauncher.java
@@ -1,5 +1,7 @@
 package ru.shanalotte.bankbarrel.webapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +12,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("ru.shanalotte.bankbarrel")
 public class WebAppLauncher {
+
+  private static final Logger logger = LoggerFactory.getLogger(WebAppLauncher.class);
+
   public static void main(String[] args) {
+    logger.info("Web app started");
     SpringApplication.run(WebAppLauncher.class, args);
   }
 }

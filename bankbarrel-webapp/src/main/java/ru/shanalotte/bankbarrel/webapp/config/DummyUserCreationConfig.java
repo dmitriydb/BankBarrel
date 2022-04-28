eed2814f4206fc062db63561651e10c27e881da2
Dmitriy Drizhilo <dmitryd@logicon-group.ru>
2022-04-28 14:56:30 +0500

    VRWN0016 создание фейковых счетов в профиле разработки

--- bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/config/DummyUserCreationConfig.java
+++ bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/config/DummyUserCreationConfig.java
@@ -5,7 +5,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.shanalotte.bankbarrel.core.domain.BankClient;
 import ru.shanalotte.bankbarrel.webapp.dao.interfaces.WebAppUserDao;
+import ru.shanalotte.bankbarrel.webapp.dto.account.AccountOpeningDto;
 import ru.shanalotte.bankbarrel.webapp.dto.bankclient.BankClientInfoDto;
+import ru.shanalotte.bankbarrel.webapp.service.BankAccountCreationService;
 import ru.shanalotte.bankbarrel.webapp.service.BankClientsEnrollingService;
 import ru.shanalotte.bankbarrel.webapp.user.WebAppUser;
 
@@ -18,18 +20,21 @@ public class DummyUserCreationConfig {
 
   private BankClientsEnrollingService bankClientsEnrollingService;
   private WebAppUserDao webAppUserDao;
+  private BankAccountCreationService bankAccountCreationService;
 
   public DummyUserCreationConfig(BankClientsEnrollingService bankClientsEnrollingService,
-                                 WebAppUserDao webAppUserDao) {
+                                 WebAppUserDao webAppUserDao,
+                                 BankAccountCreationService bankAccountCreationService) {
     this.bankClientsEnrollingService = bankClientsEnrollingService;
     this.webAppUserDao = webAppUserDao;
+    this.bankAccountCreationService = bankAccountCreationService;
   }
 
   /**
    * Создает пользователя веб-приложения и привязывает к нему банковского клиента.
    */
   @Scheduled(initialDelay = 500, fixedDelay = Integer.MAX_VALUE)
-  public void createDummyUser() {
+  public void createDummyUserAndFakeAccounts() {
     BankClientInfoDto dto = new BankClientInfoDto();
     dto.setEmail("admin@admin.ru");
     dto.setFirstName("Admin");
@@ -37,6 +42,17 @@ public class DummyUserCreationConfig {
     dto.setTelephone("+7 333 333 33 33");
     BankClient bankClient = bankClientsEnrollingService.enrollClient(dto);
     webAppUserDao.addUser(new WebAppUser("admin", bankClient));
+    AccountOpeningDto accountOpeningDto = new AccountOpeningDto();
+    accountOpeningDto.setCurrency("USD");
+    accountOpeningDto.setAccountType("CHECKING");
+    accountOpeningDto.setAccountAdditionalType("TRADITIONAL");
+    bankAccountCreationService.createAccount(accountOpeningDto, bankClient);
+    accountOpeningDto.setCurrency("RUB");
+    bankAccountCreationService.createAccount(accountOpeningDto, bankClient);
+    accountOpeningDto.setCurrency("KZT");
+    bankAccountCreationService.createAccount(accountOpeningDto, bankClient);
   }
+
+
 
 }

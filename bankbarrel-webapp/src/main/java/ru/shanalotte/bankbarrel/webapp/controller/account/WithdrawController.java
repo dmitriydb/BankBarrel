c99bfec6b37e78d11fbcea5da6714a9f58b0cc7f
Dmitriy Drizhilo <dmitryd@logicon-group.ru>
2022-04-29 12:54:52 +0500

    VRWN0020 Снятие средств со счета

---
+++ bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/controller/account/WithdrawController.java
@@ -0,0 +1,40 @@
+package ru.shanalotte.bankbarrel.webapp.controller.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.shanalotte.bankbarrel.core.domain.BankAccount;
+import ru.shanalotte.bankbarrel.core.domain.MonetaryAmount;
+import ru.shanalotte.bankbarrel.core.exception.InsufficientFundsException;
+import ru.shanalotte.bankbarrel.core.exception.UnknownCurrencyRate;
+import ru.shanalotte.bankbarrel.core.service.BankService;
+import ru.shanalotte.bankbarrel.webapp.exception.BankAccountNotExists;
+import ru.shanalotte.bankbarrel.webapp.exception.UnathorizedAccessToBankAccount;
+import ru.shanalotte.bankbarrel.webapp.exception.WebAppUserNotFound;
+import ru.shanalotte.bankbarrel.webapp.service.BankAccountAccessAuthorizationService;
+
+@Controller
+public class WithdrawController {
+
+  private BankAccountAccessAuthorizationService bankAccountAccessAuthorizationService;
+  private BankService bankService;
+
+  public WithdrawController(BankAccountAccessAuthorizationService bankAccountAccessAuthorizationService, BankService bankService) {
+    this.bankAccountAccessAuthorizationService = bankAccountAccessAuthorizationService;
+    this.bankService = bankService;
+  }
+
+  @PostMapping("/account/{number}/withdraw")
+  public String depositAccount(@PathVariable("number") String accountNumber,
+                               @RequestParam("username") String username,
+                               @RequestParam("amount") Double amount,
+                               @RequestParam("currency") String currency
+                               ) throws WebAppUserNotFound,
+      UnathorizedAccessToBankAccount, BankAccountNotExists, UnknownCurrencyRate, InsufficientFundsException {
+    BankAccount account = bankAccountAccessAuthorizationService.authorize(username, accountNumber);
+    MonetaryAmount monetaryAmount = new MonetaryAmount(amount, currency);
+    bankService.withdraw(account, monetaryAmount);
+    return "redirect:/user/" + username + "/account/" + accountNumber;
+  }
+}

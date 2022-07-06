f4cfc4000da0aef2b56a5c23361e7204a0ec7484
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-07-06 11:24:18 +0500

    BANK-2 #Done building via maven profiles

--- bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/WebAppLauncher.java
+++ bankbarrel-webapp/src/main/java/ru/shanalotte/bankbarrel/webapp/WebAppLauncher.java
@@ -2,6 +2,7 @@ package ru.shanalotte.bankbarrel.webapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,11 +14,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan("ru.shanalotte.bankbarrel")
-public class WebAppLauncher {
+public class WebAppLauncher implements CommandLineRunner {
 
   private static final Logger logger = LoggerFactory.getLogger(WebAppLauncher.class);
 
   public static void main(String[] args) {
     SpringApplication.run(WebAppLauncher.class, args);
+
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    for (int i = 0; i < 100; i++) {
+      logger.info("StartingStartingStarting... " + i);
+    }
   }
 }
